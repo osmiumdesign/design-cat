5b6fed383c7bd4bf311333dafe298da03c1feed4
osmiumdesign <dagtecinc@gmail.com>
2018-04-26 12:23:54 +0300

    add fontawesome

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
+$('.menu-hamburger').on('click', function() {
+  $(this).toggleClass('hamburger-open');
+  $('.side-menu').toggleClass('side-open');
+});
 	// Custom JS
-
+  $(window).on("load",function(){
+              $("a[rel='anchor']").mPageScroll2id({
+                scrollSpeed: 800,
+                highlightClass:"anchor-hover",
+                keepHighlightUntilNext: true,
+              });
+          });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,6 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/fontawesome/fontawesome-all.min.js',
 		'app/libs/pagescroll/PageScroll2id.min.js',
 		'app/js/common.js', // Always at the end
 		])
